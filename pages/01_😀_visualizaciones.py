1b8fe54074c5b9d05c1b7a8f8a790988a86a79cb
Francisco De Jesus Fajardo Baez <frankfajardo1978@gmail.com>
2025-02-06 21:03:14 -0500

    Update 01_😀_visualizaciones.py

--- pages/01_😀_visualizaciones.py
+++ pages/01_😀_visualizaciones.py
@@ -12,7 +12,7 @@ import sys
 
 
 if st.checkbox("mostrar texto"):
-    st.write('Frank Elias es un cagon')
+    st.write('Elias esta cagando')
 
 data = pd.read_csv('Vehicle_Fuel_Economy_Optimized (1).csv')
 
