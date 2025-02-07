be29415fb7ad50e4eb2a39bc6e73960a92c0de96
Francisco De Jesus Fajardo Baez <frankfajardo1978@gmail.com>
2025-02-07 08:55:19 -0500

    Update 01_😀_visualizaciones.py

--- pages/01_😀_visualizaciones.py
+++ pages/01_😀_visualizaciones.py
@@ -12,7 +12,7 @@ import sys
 
 
 if st.checkbox("mostrar texto"):
-    st.write('MARICO EL QUE LO LEA')
+    st.write('ALEJANDRO ROMERO ES GAY')
 
 data = pd.read_csv('Vehicle_Fuel_Economy_Optimized (1).csv')
 
